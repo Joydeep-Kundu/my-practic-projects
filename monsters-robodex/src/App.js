ebcd0348cc1167bf99cae11abce926dc999d3774
Joydeep Kundu <kjoydeep5@gmail.com>
2022-02-06 16:02:39 +0530

    adding page files for git hub pages

--- monsters-robodex/src/App.js
+++ monsters-robodex/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
   async componentDidMount() {
@@ -14,10 +16,25 @@ class App extends React.Component {
     const data = await res.json();
     this.setState({ monsters: data });
   }
+  //in func 'this' undifind and '.binding ' is used to refer to the the app
+
+  //in arrow func 'this' refer too app component
+  handleChange = (e) => (
+    this.setState({ searchField: e.target.value })
+  )
   render() {
+    const { monsters, searchField } = this.state;
+    const filterMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monster RoboDex</h1>
+        <SearchBox placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filterMonsters} />
+
+
       </div>
     )
   }
--- monsters-robodex/src/components/card/card.components.jsx
+++ monsters-robodex/src/components/card/card.components.jsx
@@ -5,5 +5,6 @@ export const Card = (props) => (
     <div className="card-container">
         <img src={`https://robohash.org/${props.monster.id}?set=set&size=180x180`} alt="monster image" />
         <h1>{props.monster.name}</h1>
+        <p>{props.monster.email}</p>
     </div>
 )
