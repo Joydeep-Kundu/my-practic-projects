7b1b5268164cfb84828f9dd3b175601261f10011
Joydeep Kundu <kjoydeep5@gmail.com>
2022-02-06 16:00:07 +0530

    Updates

---
+++ precache-manifest.3dee3e4ff22c015d57f21d8af1dd1636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bc2a597511becd4316c9d07f604adc",
+    "url": "/Monster-RoboDex/index.html"
+  },
+  {
+    "revision": "7709f314d4b167bbc0a1",
+    "url": "/Monster-RoboDex/static/css/main.60068505.chunk.css"
+  },
+  {
+    "revision": "afae7a82cc85f330b33e",
+    "url": "/Monster-RoboDex/static/js/2.1998361a.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/Monster-RoboDex/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "7709f314d4b167bbc0a1",
+    "url": "/Monster-RoboDex/static/js/main.3e54de61.chunk.js"
+  },
+  {
+    "revision": "745a46fe14c27c1a6bc2",
+    "url": "/Monster-RoboDex/static/js/runtime~main.ce6db540.js"
+  }
+]);
